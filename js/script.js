aea569d722c4b2d90c0a003b414568e194067409
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-07-21 23:53:52 +0200

    Add part of tagClickHandler function

--- js/script.js
+++ js/script.js
@@ -76,7 +76,6 @@
     // console.log(html)
     titleList.innerHTML = html;
     const links = document.querySelectorAll('.titles a');
-
     for (let link of links) {
       link.addEventListener('click', titleClickHandler);
     }
@@ -124,21 +123,23 @@
 
     function tagClickHandler(event){
       /* prevent default action for this event */
-
+      event.preventDefault();
       /* make new constant named "clickedElement" and give it the value of "this" */
+      const clickedElement = this;
 
       /* make a new constant "href" and read the attribute "href" of the clicked element */
-
+      const href = clickedElement.getAttribute('href');
       /* make a new constant "tag" and extract tag from the "href" constant */
+      const tag = href.replace('#tag-', '');
 
       /* find all tag links with class active */
-
+      const activeLinks = document.querySelectorAll('a.active[href^="#tag-"]');
       /* START LOOP: for each active tag link */
-
+        for (let activeLink of activeLinks) {
         /* remove class active */
-
+        activeLink.classList.remove('active');
       /* END LOOP: for each active tag link */
-
+        }
       /* find all tag links with "href" attribute equal to the "href" constant */
 
       /* START LOOP: for each found tag link */
@@ -148,7 +149,7 @@
       /* END LOOP: for each found tag link */
 
       /* execute function "generateTitleLinks" with article selector as argument */
-    }
+    };
 
     function addClickListenersToTags(){
       /* find all links to tags */
