ccfb314b6107eca0b536cf72b756b034a454b0b3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2022-07-24 19:57:07 +0200

    Add cod generating tags with count

--- js/script.js
+++ js/script.js
@@ -132,8 +132,17 @@
       /* [NEW] find list of tags in right column */
       const tagList = document.querySelector(optTagsListSelector);
 
-      /* [NEW] add html from allTags to tagList */
-      tagList.innerHTML = allTags.join(' ');
+      /* [NEW] create variable for all links HTML code */
+      let allTagsHTML = '';
+
+      /* [NEW] START LOOP: for each tag in allTags: */
+      for(let tag in allTags){
+        /* [NEW] generate code of a link and add it to allTagsHTML */
+        allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+      /* [NEW] END LOOP: for each tag in allTags: */
+      }
+      /*[NEW] add HTML from allTagsHTML to tagList */
+      tagList.innerHTML = allTagsHTML;
     }
 
     function tagClickHandler(event){
